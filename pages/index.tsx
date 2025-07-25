f922f16a1ff43bf48f2099fea907e666e963c882
chamara-launchpad <chamara@libt.co.uk>
2025-07-25 14:41:35 +0100

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
-// Home Page
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to the Isle of Man Events App</h1>
+      <p><a href="/events">View All Events</a></p>
+    </div>
+  )
+}
